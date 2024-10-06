5add55cc330bb1782c0197ee81d5444d7779885b
saifshamil <saifshamil141@gmail.com>
2024-10-06 11:58:39 +0500

    second assignment

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 
-const Card = (props) => {
+interface Tobj{
+  Name:string,
+  RollNo:number,
+  Day:string
+}
+const Card = (props:Tobj) => {
   return (
+    <div className='bg-slate-400 p-10 w-96 m-10 rounded-2xl font-extrabold outline
+   hover:scale-105 transform transition-transform  duration-500
+    '>
     <div>
-      <h1>Name:{props.Name}</h1>
-      <h1>RollNo.{props.RollNo}</h1>
+      <h1>Name: {props.Name}</h1>
+      
+      </div>
+      <div>
+      <h1>RollNo. {props.RollNo}</h1>
+      </div>
+      <div>
       <h1>Day : {props.Day}</h1>
-    </div>
+      </div>
+      </div>
+
   )
 }
 
